054f813db75ba6294419da6e17eaaf9b6630aee5
dzhukov40 <dzhukov40.v@mail.ru>
2018-09-16 03:19:25 +0300

    добавили тестовый контроллер

---
+++ src/main/java/ru/doneathome/boomberman/startPoint/EchoController.java
@@ -0,0 +1,18 @@
+package ru.doneathome.boomberman.startPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/echo")
+public class EchoController {
+    private static final Logger log = LoggerFactory.getLogger(EchoController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody String getPipe() {
+        log.info("был запросик");
+
+        return "приветики";
+    }
+}
--- src/main/java/ru/doneathome/boomberman/startPoint/StartApplication.java
+++ src/main/java/ru/doneathome/boomberman/startPoint/StartApplication.java
@@ -2,6 +2,8 @@ package ru.doneathome.boomberman.startPoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // запустить сервер из консольки в проектре [ java -jar ./build/libs/boomberman.jar ]
 // топаем на localhost:8070 - порт проставляется в [ applicapion.properties ]
