83bc9dfa7c962e49413119465c3a3d6ab780cdad
dzhukov40 <dzhukov40.v@mail.ru>
2018-11-23 22:01:55 +0300

    убираем вообще это ограничение пока что

--- src/main/java/ru/doneathome/boomberman/controller/controllerImpl/BaseControllerImpl.java
+++ src/main/java/ru/doneathome/boomberman/controller/controllerImpl/BaseControllerImpl.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // разрешение CORS нам нужно только для запуска [ node ./debugStaticServer.js ]
-@CrossOrigin(origins = "http://127.0.0.1:3000") //TODO: перенести в properties
+@CrossOrigin(origins = "*") //TODO: надо при отладки фронта при помощи сервера на node
 public class BaseControllerImpl {
 
 }
